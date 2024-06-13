b9ce807afb461c3df2c7bcfa299d6051b396ef07
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-13 17:06:30 +0200

    iteration 7

--- src/chronometer.js
+++ src/chronometer.js
@@ -12,27 +12,41 @@ class Chronometer {
       if (printTimeCallback) {
         printTimeCallback();
       }
-    });
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (typeof value !== "number") {
+      return null;
+    }
+
+    let stringValue = `${value}`;
+    if (stringValue.length === 1) {
+      stringValue = `0${value}`;
+    }
+
+    return stringValue;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
